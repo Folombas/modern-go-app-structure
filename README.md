b4cbe86a13cef0e0b8eb16ccf4e7b7b48a82be43
Folombas <folombas@yandex.ru>
2025-07-13 00:09:54 +0300

    regeneration

--- README.md
+++
@@ -1,7 +0,0 @@
-cat > README.md << 'EOF'
-
-# Modern Go Application Structure
-
-Проект для изучения современной структуры Go-приложения
-
-## Структура проекта
--- scripts/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "🔄 Применяем миграции БД..."
-PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f /app/migrations/001_init_schema.up.sql
-echo "🚀 Запускаем приложение..."
-./delivery-app
