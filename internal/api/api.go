e195e40d8833c682ff316ade0b7ad7e237fbbf7c
Folombas <folombas@yandex.ru>
2025-07-13 20:47:41 +0300

    golangci-lint successfully installed

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,14 +1,20 @@
-// internal/api/api.go
 package api
 
 import (
     "fmt"
+    "log"
     "net/http"
 )
 
 func Run() error {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello from Modern-go-app-structure!")
+        // Теперь проверяем ошибку
+        _, err := fmt.Fprintf(w, "Hello from Modern-go-app-structure!")
+        if err != nil {
+            log.Printf("Error writing response: %v", err)
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
     })
     return http.ListenAndServe(":8080", nil)
 }
