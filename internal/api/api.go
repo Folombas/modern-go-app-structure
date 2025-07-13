372704d2fc39247fc8eaccbe918500d691252111
Folombas <folombas@yandex.ru>
2025-07-13 22:21:23 +0300

    created module useragent

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,19 +2,29 @@ package api
 
 import (
     "fmt"
-    "log"
     "net/http"
+    "modern-go-app-structure/internal/api/useragent"
 )
 
 func Run() error {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        // Теперь проверяем ошибку
-        _, err := fmt.Fprintf(w, "Hello from Modern-go-app-structure!")
-        if err != nil {
-            log.Printf("Error writing response: %v", err)
-            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-            return
-        }
+        // Получаем User-Agent из запроса
+        userAgent := r.UserAgent()
+        
+        // Получаем информацию о клиенте
+        clientInfo := useragent.GetClientInfo(userAgent)
+        
+        // Формируем ответ
+        message := fmt.Sprintf(`
+            <h1>Ваши данные:</h1>
+            <p>Операционная система: %s</p>
+            <p>Браузер: %s</p>
+            <p>Версия браузера: %s</p>
+        `, clientInfo.OS, clientInfo.Browser, clientInfo.Version)
+        
+        // Выводим результат
+        w.Write([]byte(message))
     })
+    
     return http.ListenAndServe(":8080", nil)
 }
